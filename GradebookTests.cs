851a27166fc83135c7ede3c298109dff5372e980
Ryn <wykoffrachel@gmail.com>
2026-02-03 16:10:29 -0600

    Add files via upload
    File to ensure the program works correctly.

---
+++ GradebookTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GradebookApp;
+
+namespace Gradebook.Tests
+{
+    public class GradebookTests
+    {
+        //Sucessfully add a valid grade
+        [Fact]
+
+        public void AddGrade_AddsValidGrade()
+        {
+            var book = new GradebookApp.Gradebook();
+            book.AddGrade(90);
+            Assert.Equal(1, book.GetCount());
+        }
+
+        //Reject an invalid grade
+        [Fact]
+        public void AddGrade_RejectsInvalidGrade() 
+        {
+            var book = new GradebookApp.Gradebook();
+            Assert.Throws<ArgumentOutOfRangeException>(  () => book.AddGrade(150));
+        }
+
+        //Get the correct average
+        [Fact]
+        public void GetAverage_ReturnsCorrectAverage()
+        {
+            var book = new GradebookApp.Gradebook();
+            book.AddGrade(new List<double> { 80, 90, 100 });
+            Assert.Equal(90, book.GetAverage(), 2);
+
+        }
+
+
+        //Validate the highest and lowest
+        [Fact]
+        public void GetHighestAndLowest_WorksCorrectly()
+        {
+            var book = new GradebookApp.Gradebook();
+            book.AddGrade(new List<double> { 70, 85, 95 });
+            Assert.Equal(95, book.GetHighest());
+            Assert.Equal(70, book.GetLowest());
+        }
+    }
+}
